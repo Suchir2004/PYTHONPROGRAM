735bea3d0f93ef48254202166b56eb9ae39c584f
SUCHIR KAUSHIK <113572013+Suchir2004@users.noreply.github.com>
2024-02-14 22:50:29 +0530

    Delete buzzcheck.py

--- buzzcheck.py
+++
@@ -1,9 +0,0 @@
-def is_buzz_number(num):
-    return num % 7 == 0 or num % 10 == 7
-
-number = int(input("Enter a number: "))
-
-if is_buzz_number(number):
-    print(number, "is a buzz number.")
-else:
-    print(number, "is not a buzz number.")
